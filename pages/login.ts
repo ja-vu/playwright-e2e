a3a138f2a77568cb00a64f6ac9d27f48fe42f04b
James Vu <jamesvu@Jamess-MacBook-Pro.local>
2023-08-11 21:54:41 -0400

    added test case for failed login

--- pages/login.ts
+++ pages/login.ts
@@ -1,16 +1,18 @@
-import { expect, type Locator, type Page } from '@playwright/test';
+import { ElementHandle, expect, type Locator, type Page } from '@playwright/test';
 
 export class LoginPage {
   readonly page: Page;
   readonly getUsernameInput: Locator;
   readonly getPasswordInput: Locator;
   readonly getLoginButton: Locator;
+  readonly errorMessage: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.getUsernameInput = page.locator('[data-test="username"]');
     this.getPasswordInput = page.locator('[data-test="password"]');
     this.getLoginButton = page.locator('[data-test="login-button"]');
+    this.errorMessage = page.locator('[data-test="error"]');
   }
 
   async navigate() {
@@ -22,4 +24,9 @@ export class LoginPage {
     await this.getPasswordInput.fill(password);
     await this.getLoginButton.click();
   }
+
+  async isLoginFailed(){
+    const errorMsg = 'Epic sadface: Username and password do not match any user in this service'
+    await expect(this.errorMessage).toHaveText(errorMsg)
+}
 }
--- tests/first.spec.ts
+++ tests/first.spec.ts
@@ -11,3 +11,15 @@ test('Logging in', async ({ page }) => {
     await expect(page).toHaveTitle(/Swag Labs/);
     await loginPage.login('standard_user', 'secret_sauce')
   });
+
+  test('Failed logging in', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    const errorMsg = 'Epic sadface: Username and password do not match any user in this service'
+      
+      await loginPage.navigate();
+  
+      // Expect a title "to contain" a substring.
+      await expect(page).toHaveTitle(/Swag Labs/);
+      await loginPage.login('standard_user', 'abcd1234');
+      await loginPage.isLoginFailed();
+    });
